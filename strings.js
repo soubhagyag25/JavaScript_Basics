451e6a2575e728fc2b58f1847fcfe0ff544251e1
Soubhagya Gautam <soubhagyag25@gmail.com>
2024-06-27 11:14:55 +0530

    adding some comments in strings.js

--- strings.js
+++ strings.js
@@ -19,3 +19,9 @@ console.log(name.concat(" is a friend of ",friend))
 for(let i=0;i<name.length;i++)
     console.log(name[i])
 //note: Strings are immutable i.e cannot be changed
+// use of .includes f()
+//.includes() is a boolean f() which returns true or false if the word in present in the string
+let sentence="Hello! How are you?"
+console.log(sentence.includes("Hello")) // true
+console.log(sentence.startsWith("Hello"))
+console.log(sentence.endsWith("?"))
